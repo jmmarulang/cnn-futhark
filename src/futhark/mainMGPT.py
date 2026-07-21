4293933c5e20b6b38952440c5613e384a00e7b9d
jmmarulang <78051861+jmmarulang@users.noreply.github.com>
2026-07-21 17:04:19 +0100

    ss

--- src/futhark/mainMGPT.py
+++ src/futhark/mainMGPT.py
@@ -260,8 +260,8 @@ mpprobs = np.array([mp.softmax(logits) for logits in pmlogits])
 # plt.show()
 
 barWidth = 0.25
-lfprobs = mfprobs[-1]
-lpprobs = mpprobs[-1]
+lfprobs = mfprobs[0]
+lpprobs = mpprobs[0]
 
 br1 = np.arange(len(lfprobs))
 br2 = [x + barWidth for x in br1]
